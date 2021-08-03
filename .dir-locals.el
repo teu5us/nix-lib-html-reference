e39a3d0f99ba5bea760f30aa956e077ccd1d57ce
Pavel Stepanov <pavelstepanov@vivaldi.net>
2021-08-03 13:14:16 +0600

    use bundled lisp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lisp-mode . ((inferior-lisp-program . "ros run"))))
+((lisp-mode . ((inferior-lisp-program . "lisp"))))
